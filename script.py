db6ed13283ca09376daef9bcd5b7524e726a8fbc
kproudler <57147062+kproudler@users.noreply.github.com>
2022-01-26 10:07:56 -0500

    plot of rev + exp

--- script.py
+++ script.py
@@ -7,3 +7,24 @@ import numpy as np
 # load in financial data
 financial_data = pd.read_csv('financial_data.csv')
 
+print(financial_data.head())
+
+month = financial_data.Month
+revenue = financial_data.Revenue
+expenses = financial_data.Expenses
+
+# Examination of revenue and expenses over the past 6 months
+
+plt.plot(month, revenue)
+plt.xlabel('Month')
+plt.title('Revenue')
+plt.ylabel('Amount ($)')
+plt.show()
+
+plt.clf()
+plt.plot(month, expenses)
+plt.title('Expenses')
+plt.xlabel('Expenses')
+plt.ylabel('Amount ($)')
+plt.show()
+
